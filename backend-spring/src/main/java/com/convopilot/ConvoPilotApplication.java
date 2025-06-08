6d48a735478b10f958b7b824f19d18fa71ad0951
Aaron Yuan <85452885+a-ayuan@users.noreply.github.com>
2025-06-08 18:20:07 -0400

    fix: update application

--- backend-spring/src/main/java/com/convopilot/ConvoPilotApplication.java
+++ backend-spring/src/main/java/com/convopilot/ConvoPilotApplication.java
@@ -4,14 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 @EnableMongoAuditing
 public class ConvoPilotApplication {
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(ConvoPilotApplication.class, args);
     }
 }
